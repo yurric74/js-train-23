8a48c7f46775a55fd832a48ea8757a0eb3a0d49b
unknown <yurric@ukr.net>
2023-09-15 17:00:28 +0300

    js-train-23

--- 3.js
+++ 3.js
@@ -3,32 +3,40 @@
 
 // Клас TeaMaker відповідає за загальні дії, необхідні для приготування чаю.
 class TeaMaker {
+  makeTea (){
+  this.boilWater(); this.addTeaLeaves(); this.steepTea();
+  this.pourIntoCup(); this.addCondiments(); this.serveTea();}
+  
   // Робимо метод makeTea, який викликає всі кроки приготування чаю по черзі boilWater, addTeaLeaves, #steepTea,
   // pourIntoCup, addCondiments, serveTea.
-  // Робимо метод boilWater, який відповідає за кип'ятіння води та виводить в консоль Кип'ятимо воду....
-  // Робимо метод addTeaLeaves, який відповідає за додавання чайних листків та виводить в консоль Додаємо чайні листки....
-  // Робимо метод steepTea, що відповідає за заварювання чаю та виводить в консоль Заварюємо чай....
-  // Робимо метод pourIntoCup, що відповідає за переливання чаю в чашку та виводить в консоль Переливаємо чай в чашку....
-  // Робимо метод addCondiments, що залишається пустим і може бути перевизначений у підкласах.
-  // Робимо метод serveTea, що відповідає за подачу чаю та виводить в консоль Чай подається!.
+  boilWater () {console.log(`Кип'ятимо воду....`);}// Робимо метод boilWater, який відповідає за кип'ятіння води та виводить в консоль Кип'ятимо воду....
+  addTeaLeaves() {console.log(`Додаємо чайні листки....`);}// Робимо метод addTeaLeaves, який відповідає за додавання чайних листків та виводить в консоль Додаємо чайні листки....
+  steepTea () {console.log(`Заварюємо чай....`);}// Робимо метод steepTea, що відповідає за заварювання чаю та виводить в консоль Заварюємо чай....
+  pourIntoCup () {console.log(`Переливаємо чай в чашку....`);}// Робимо метод pourIntoCup, що відповідає за переливання чаю в чашку та виводить в консоль Переливаємо чай в чашку....
+  addCondiments () {}// Робимо метод addCondiments, що залишається пустим і може бути перевизначений у підкласах.
+  serveTea () {console.log(`Чай подається!`);}// Робимо метод serveTea, що відповідає за подачу чаю та виводить в консоль Чай подається!.
 }
 
 // Клас GreenTeaMaker є підкласом класу TeaMaker та додає інгредієнти для зеленого чаю.
 class GreenTeaMaker extends TeaMaker {
-  // Робимо метод addCondiments, який виводить в консоль Додаємо мед, щоб приготувати зелений чай...
+  addCondiments () {
+    console.log(`Додаємо мед, щоб приготувати зелений чай...`);
+  }// Робимо метод addCondiments, який виводить в консоль Додаємо мед, щоб приготувати зелений чай...
 }
 
 // Клас BlackTeaMaker є підкласом класу TeaMaker та додає інгредієнти для чорного чаю.
 class BlackTeaMaker extends TeaMaker {
-  // Робимо метод addCondiments, який виводить в консоль Додаємо мед, щоб приготувати чорний чай...
+  addCondiments() {
+    console.log(`Додаємо мед, щоб приготувати чорний чай...`);
+  }// Робимо метод addCondiments, який виводить в консоль Додаємо мед, щоб приготувати чорний чай...
 }
 
 console.log("Завдання 3 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створюємо екземпляри класів GreenTeaMaker та BlackTeaMaker.
-// const greenTeaMaker = new GreenTeaMaker();
-// greenTeaMaker.makeTea();
+ const greenTeaMaker = new GreenTeaMaker();
+ greenTeaMaker.makeTea();
 
-// const blackTeaMaker = new BlackTeaMaker();
-// blackTeaMaker.makeTea();
+ const blackTeaMaker = new BlackTeaMaker();
+ blackTeaMaker.makeTea();
--- 5.js
+++ 5.js
@@ -3,11 +3,15 @@
 
 // Клас BankTransfer представляє собою систему для здійснення банківських переказів
 class BankTransfer {
+  initiateTransfer(amount) {
+    const calculatedAmount = this.calculateFee(amount);
+    console.log(`Ініціюємо банківський переказ: $${calculatedAmount}`);}
   // Зробіть метод initiateTransfer, який приймає amount та відповідає за ініціювання банківського переказу
   // Він приймає суму переказу як параметр
   // Для ініціювання банківського переказу спершу обчислюється сума з урахуванням комісії calculatedAmount = this.calculateFee(amount)
   // Виводимо інформацію про ініціювання банківського переказу Ініціюємо банківський переказ: $${calculatedAmount}
-  // Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
+  calculateFee(amount) {
+    return amount * 1.02;} // 2% комісія// Зробіть метод calculateFee, який відповідає за розрахунок комісії за переказ
   // Він приймає amount переказу як параметр та повертає число після розрахування комісії
   // Логіка розрахунку комісії за переказ amount * 1.02
   // Припустимо, комісія становить 2% від суми переказу
@@ -15,6 +19,8 @@ class BankTransfer {
 
 // Клас WalletTransfer представляє собою систему для здійснення переказів з гаманця
 class WalletTransfer {
+  processTransfer(amount) {
+    console.log(`Здійснюємо переказ з гаманця: $${amount}`);}
   // Створіть метод processTransfer, який відповідає за здійснення переказу з гаманця
   // Він приймає суму переказу як параметр
   // Виводимо інформацію про здійснення переказу з гаманця Здійснюємо переказ з гаманця: $${amount}
@@ -23,22 +29,27 @@ class WalletTransfer {
 // Клас TransferAdapter виступає адаптером, який дозволяє нам користуватися
 // методами WalletTransfer так, ніби це BankTransfer.
 class TransferAdapter {
+  constructor(transferSystem) {
+    this.transferSystem = transferSystem;}
   // Робимо конструктор, що приймає об'єкт transferSystem типу WalletTransfer
   // Зберігаємо посилання на об'єкт WalletTransfer у властивості transferSystem
-  // Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
+  initiateTransfer(amount) {
+    const calculatedAmount = this.calculateFee(amount);
+    this.transferSystem.processTransfer(calculatedAmount);}// Робимо метод initiateTransfer, який адаптує API WalletTransfer до API BankTransfer.
   // Він приймає amount як аргумент та повертає результат виконання переказу.
   // Викликаємо допоміжний метод calculateFee для обчислення комісії за переказ та результат записуєм в константу calculatedAmount
   // Викликаємо метод processTransfer об'єкту WalletTransfer з calculatedAmount.
   // В результаті повертаємо результат виконання переказу.
-  // Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
+  calculateFee(amount) {
+    return amount * 1.02;}// Створюємо метод calculateFee, що приймає amount та обчислює суму комісії за переказ amount * 1.2, засновуючись на вхідній сумі.
   // Повертаємо amount * 1.2
 }
 console.log("Завдання 5 ====================================");
 // Після виконання розкоментуйте код нижче
 
 // Створимо екземпляри BankTransfer
-// const purchase1 = new BankTransfer();
-// purchase1.initiateTransfer(1000);
+ const purchase1 = new BankTransfer();
+ purchase1.initiateTransfer(1000);
 
-// const purchase2 = new BankTransfer();
-// purchase2.initiateTransfer(10);
+ const purchase2 = new BankTransfer();
+ purchase2.initiateTransfer(10);
